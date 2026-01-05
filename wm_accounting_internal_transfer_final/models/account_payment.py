332f978a843b873a6e09c28e0963e59ed70ef205
Mohamed Abdalazem <mohamedabdalazemalawad@gmail.com>
2026-01-05 12:18:46 +0200

    Update module

--- wm_accounting_internal_transfer_final/models/account_payment.py
+++ wm_accounting_internal_transfer_final/models/account_payment.py
@@ -1,63 +1,43 @@
-from odoo import models, fields, _
-from odoo.exceptions import UserError
+def action_post(self):
+    for payment in self:
+        # تحقق أولاً إذا كان تحويل داخلي بدون اختيار الحساب الوجهة
+        if payment.is_internal_transfer and not payment.destination_journal_id:
+            raise UserError(_("الرجاء اختيار الحساب الوجهة."))
 
-class AccountPayment(models.Model):
-    _inherit = 'account.payment'
+        if payment.is_internal_transfer:
+            if payment.journal_id == payment.destination_journal_id:
+                raise UserError(_("الحساب المصدر والوجهة يجب أن يكونا مختلفين."))
 
-    # حقل التحويل الداخلي
-    is_internal_transfer = fields.Boolean(string="تحويل داخلي")
+            transfer_account = payment.journal_id.company_id.transfer_account_id
+            if not transfer_account:
+                raise UserError(_("الرجاء تفعيل حساب Liquidity Transfer في إعدادات الشركة."))
 
-    # حساب الوجهة
-    destination_journal_id = fields.Many2one(
-        'account.journal',
-        string="إلى الحساب",
-        domain=[('type', 'in', ('bank', 'cash'))]
-    )
+            move_vals = {
+                'date': payment.date,
+                'ref': _('تحويل داخلي %s → %s') % (
+                    payment.journal_id.name,
+                    payment.destination_journal_id.name
+                ),
+                'line_ids': [
+                    (0, 0, {
+                        'account_id': payment.journal_id.default_account_id.id,
+                        'credit': payment.amount,
+                        'name': _('تحويل داخلي'),
+                    }),
+                    (0, 0, {
+                        'account_id': payment.destination_journal_id.default_account_id.id,
+                        'debit': payment.amount,
+                        'name': _('تحويل داخلي'),
+                    }),
+                ],
+            }
 
-    def action_post(self):
-        for payment in self:
-            # تحقق أولاً إذا كان تحويل داخلي بدون اختيار الحساب الوجهة
-            if payment.is_internal_transfer and not payment.destination_journal_id:
-                raise UserError(_("الرجاء اختيار الحساب الوجهة."))
+            move = self.env['account.move'].create(move_vals)
+            move.action_post()
 
-            # إذا كان تحويل داخلي
-            if payment.is_internal_transfer:
-                # التحقق أن الحساب المصدر والوجهة مختلفان
-                if payment.journal_id == payment.destination_journal_id:
-                    raise UserError(_("الحساب المصدر والوجهة يجب أن يكونا مختلفين."))
+            # بدل السطر القديم
+            payment.post()  # الطريقة الصحيحة لتغيير الحالة إلى 'posted'
 
-                # التأكد من وجود حساب التحويل في إعدادات الشركة
-                transfer_account = payment.journal_id.company_id.transfer_account_id
-                if not transfer_account:
-                    raise UserError(_("الرجاء تفعيل حساب Liquidity Transfer في إعدادات الشركة."))
-
-                # إنشاء القيد المحاسبي للتحويل الداخلي
-                move_vals = {
-                    'date': payment.date,
-                    'ref': _('تحويل داخلي %s → %s') % (
-                        payment.journal_id.name,
-                        payment.destination_journal_id.name
-                    ),
-                    'line_ids': [
-                        (0, 0, {
-                            'account_id': payment.journal_id.default_account_id.id,
-                            'credit': payment.amount,
-                            'name': _('تحويل داخلي'),
-                        }),
-                        (0, 0, {
-                            'account_id': payment.destination_journal_id.default_account_id.id,
-                            'debit': payment.amount,
-                            'name': _('تحويل داخلي'),
-                        }),
-                    ],
-                }
-
-                move = self.env['account.move'].create(move_vals)
-                move.action_post()
-                payment.state = 'posted'
-
-            # إذا لم يكن تحويل داخلي، استخدم الطريقة الافتراضية
-            else:
-                super(AccountPayment, payment).action_post()
-
-        return True
+        else:
+            super(AccountPayment, payment).action_post()
+    return True
