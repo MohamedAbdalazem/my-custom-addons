7fb8e4eb2d50807034baeaef95e8e543ae722c66
Mohamed Abdalazem <mohamedabdalazemalawad@gmail.com>
2026-01-08 18:04:53 +0200

    Update module

---
+++ auto_internal_transfer_reconcile_safe/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Auto Internal Transfer Reconcile (Safe)',
+    'version': '1.1',
+    'category': 'Accounting',
+    'summary': 'Safely auto reconcile internal transfers (skip if accounts differ)',
+    'depends': ['account'],
+    'author': 'Custom',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
